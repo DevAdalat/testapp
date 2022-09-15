83d095f456a908d4bd36fd0e9c9e7d285cddcb1c
DevAdalat <akbusiness022@gmail.com>
2022-09-15 16:48:00 +0530

    Bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -15,119 +16,160 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: "TestApp",
+      title: "Test App",
       theme: ThemeData(
+        useMaterial3: true,
         primarySwatch: Colors.red,
       ),
-      home: const FileTree(),
+      home: const Home(),
     );
   }
 }
 
-class FileTree extends StatelessWidget {
-  const FileTree({super.key});
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Get.put(FileTreeController());
-    return GetBuilder<FileTreeController>(
-      builder: ((controller) {
-        return Scaffold(
+    Get.put(HomeController());
+    return GetBuilder<HomeController>(builder: (controller) {
+      ScrollController scrollController = ScrollController();
+      return WillPopScope(
+        onWillPop: (() async {
+          if (controller.currnetPath == "/storage/emulated/0") {
+            SystemNavigator.pop(animated: true);
+            return true;
+          } else {
+            final aaa = Directory(controller.currnetPath);
+            controller.getFiles(aaa.parent.path);
+            controller.update();
+            return false;
+          }
+        }),
+        child: Scaffold(
           body: CustomScrollView(
               physics: const BouncingScrollPhysics(),
+              controller: scrollController,
               slivers: [
-                SliverAppBar.medium(
-                  title: const Text("Internal Storage"),
-									actions: [
-										IconButton(
-											onPressed: (() async {
-												final storage = await Permission.storage.status;
-												final manageStoarage = await Permission.manageExternalStorage.status;
-												if (storage.isDenied || manageStoarage.isDenied) {
-												await	Permission.storage.request();
-												await Permission.manageExternalStorage.request();
-												} else {
-													Get.snackbar("Info", "Permission is already granted");
-												}
-											}),
-											icon: const Icon(Icons.settings_phone_rounded),
-											)
-									]
-                ),
+                SliverAppBar.large(
+                    leading: IconButton(
+                        onPressed: (() {
+                          if (controller.currnetPath == "/storage/emulated/0") {
+                            SystemNavigator.pop(animated: true);
+                            SystemSound.play(SystemSoundType.click);
+                          } else {
+                            final aaa = Directory(controller.currnetPath);
+                            controller.getFiles(aaa.parent.path);
+                            controller.update();
+                          }
+                        }),
+                        icon: const Icon(Icons.arrow_back)),
+                    title: const Text("Simple File Manager"),
+                    actions: [
+                      IconButton(
+                        onPressed: (() async {
+                          final storageStaus = await Permission.storage.status;
+                          final managePermission =
+                              await Permission.manageExternalStorage.status;
+                          if (storageStaus.isDenied ||
+                              managePermission.isDenied) {
+                            await Permission.storage.request();
+                            await Permission.manageExternalStorage.request();
+                          } else {
+                            final list1 =
+                                Directory("/storage/emulated/0").listSync();
+                            controller.allFiles = list1;
+                            controller.update();
+                            Get.snackbar("Path",
+                                "${await getExternalStorageDirectories()}");
+                            Get.snackbar("Path2",
+                                "${await getExternalStorageDirectories(type: StorageDirectory.dcim)}");
+                          }
+                        }),
+                        icon: const Icon(Icons.more_vert),
+                      ),
+                    ]),
                 SliverList(
                   delegate: SliverChildListDelegate([
                     ListView.separated(
-                      shrinkWrap: true,
                       physics: const NeverScrollableScrollPhysics(),
-                      itemCount: controller.directores.length,
+                      shrinkWrap: true,
                       itemBuilder: ((context, index) {
                         return ListTile(
-                            title: Text(controller.directores[index]),
-                            onTap: (() {
-                              if (FileSystemEntity.isDirectorySync(
-                                  "${controller.currentPath}/${controller.directores[index]}")) {
-                                controller.getDirs(Directory(
-                                    "${controller.currentPath}/${controller.directores[index]}"));
-                                controller.update();
-                              } else {
-                                Get.snackbar("Info", "This is a file");
-                              }
-                            }));
+                          title: Text(controller.allFiles[index].path
+                              .replaceAll("${controller.currnetPath}/", "")),
+                          leading: controller.allFiles[index] is Directory
+                              ? const Icon(Icons.folder_outlined)
+                              : controller.allFiles[index].path.isAPKFileName
+                                  ? const Icon(Icons.android_rounded)
+                                  : const Icon(Icons.file_copy),
+                          onTap: (() {
+                            if (controller.allFiles[index] is Directory) {
+                              controller
+                                  .getFiles(controller.allFiles[index].path);
+                              scrollController.jumpTo(0);
+                              controller.update();
+                            } else {
+                              Get.snackbar("Info", "This is a File");
+                            }
+                          }),
+                        );
                       }),
+                      itemCount: controller.allFiles.length,
                       separatorBuilder: ((context, index) {
-                        return const Divider();
+                        return const CustomDivider();
                       }),
-                    )
+                    ),
                   ]),
                 )
               ]),
-        );
-      }),
-    );
+        ),
+      );
+    });
   }
 }
 
-class FileTreeController extends GetxController {
-  String currentPath = "";
-  List<String> directores = <String>[];
-
-  void getDirs(Directory path) {
-		try {
-	  currentPath = path.path;
-    final directory = path.listSync();
-    directores = directory.map(((e) => e.path)).toList();
-    final pureDirs =
-        directores.map(((e) => e.replaceAll("$currentPath/", ""))).toList();
-    directores.clear();
-    directores = pureDirs;
-		} catch(e) {
-			final parentPath = path.parent;
-			currentPath = parentPath.path;
-			update();
-		}
- }
-
-  Future<List<Directory>> getStorageList() async {
-    List<Directory> paths = (await getExternalStorageDirectories())!;
-    List<Directory> filteredPaths = <Directory>[];
-    for (Directory dir in paths) {
-      filteredPaths.add(removeDataDirectory(dir.path));
+class HomeController extends GetxController {
+  List<FileSystemEntity> allFiles = [];
+  List<String> fileTime = [];
+  String currnetPath = "";
+  getFiles(String path) {
+    currnetPath = path;
+    final dir = Directory(path);
+    try {
+      final listDirs = dir.listSync();
+      allFiles = listDirs;
+    } catch (e) {
+      currnetPath = dir.parent.path;
+      update();
+      Get.snackbar("Error", e.toString());
     }
-    return filteredPaths;
-  }
-
-  Directory removeDataDirectory(String path) {
-    return Directory(path.split('Android')[0]);
-  }
-
-  handleStorage() async {
-    final path = await getStorageList();
-    getDirs(path.first);
   }
 
   @override
   onInit() {
-    handleStorage();
+    getFiles("/storage/emulated/0");
     super.onInit();
+  }
+}
+
+class CustomDivider extends StatelessWidget {
+  const CustomDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.centerRight,
+          child: Container(
+            height: 1,
+            color: Theme.of(context).dividerColor,
+            width: size.width - 70,
+          ),
+        ),
+      ],
+    );
   }
 }
