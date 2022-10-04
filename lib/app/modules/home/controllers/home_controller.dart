4dc42b19724e32f6c5277099fb5ba49db50df000
DevAdalat <akbusiness022@gmail.com>
2022-10-04 09:27:57 +0530

    Isolate testing

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,10 +1,22 @@
+import 'dart:async';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController
     with GetSingleTickerProviderStateMixin {
   late Animation<double> animation;
+  RxString count = "".obs;
   late AnimationController animationController;
+
+	void recieve(){
+		ReceivePort port = ReceivePort();
+		final isolate = Isolate.spawn(runTimer, port.sendPort);
+		port.listen(((message){
+			count.value = message.toString();
+		}));
+	}
 
   @override
   onInit() {
@@ -16,5 +28,14 @@ class HomeController extends GetxController
         CurvedAnimation(curve: Curves.easeInOut, parent: animationController);
     animation = Tween<double>(begin: 0, end: 1).animate(curvedAnimation);
     super.onInit();
+  }
+
+  void runTimer(SendPort sendPort) {
+    int count = 0;
+    Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      count++;
+      String msg = 'notification $count';
+      sendPort.send(msg);
+    });
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,33 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:telegram_client/telegram_client.dart';
-
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+		Get.put(HomeController());
     return Scaffold(
-        body: Center(
+      body: Column(
+        children: [
+          Center(
             child: ElevatedButton(
-      onPressed: (() async {
-        try {
-          const path = "/storage/emulated/0";
-          Tdlib tg = Tdlib("libtdjson.so", clientOption: {
-            'api_id': 12860793,
-            'api_hash': '668c3c09dc67819c912c918db5648cf1',
-            'database_directory': "$path/user/",
-            'files_directory': "$path/user/",
-          });
-          final me = await tg.getMe(clientId: 1570907697);
-          Get.snackbar("Me", me.toString());
-          await tg.initIsolate();
-        } catch (e) {
-          Get.snackbar("Error", e.toString());
-        }
-      }),
-      child: const Text("Get Data"),
-    )));
+              onPressed: (() async {
+                try {
+                  controller.recieve();
+                } catch (e) {
+                  Get.snackbar("Error", e.toString());
+                }
+              }),
+              child: const Text("Get Data"),
+            ),
+          ),
+          Obx((() => Text(controller.count.value))),
+        ],
+      ),
+    );
   }
 }
