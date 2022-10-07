7ab12fadd26c492a494e3cda635d8d54513413e9
DevAdalat <akbusiness022@gmail.com>
2022-10-07 10:10:00 +0530

    Testing

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,7 +6,7 @@ use std::{
 use walkdir::WalkDir;
 
 #[no_mangle]
-fn getallcount(path: *mut c_char) -> c_int {
+pub extern "C" fn getallcount(path: *mut c_char) -> c_int {
     let c_str = unsafe { CStr::from_ptr(path) };
     let str_slice = c_str.to_str().unwrap();
     let count = thread::spawn(move || WalkDir::new(str_slice).into_iter().count());
