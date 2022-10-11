69ae0d4ab1c6e71f2fc2ed4454f450355c8b6afd
DevAdalat <akbusiness022@gmail.com>
2022-10-11 15:01:58 +0530

    Testing

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -64,7 +64,6 @@ fn get_total_size_of_images() {
                 || file_name.ends_with(".jpeg")
             {
                 size = size + entry.metadata().unwrap().len();
-                env::set_var("PNG_SIZE", size.to_string());
                 fs::write(
                     "/storage/emulated/0/size.txt",
                     size.to_string().into_bytes(),
@@ -72,5 +71,6 @@ fn get_total_size_of_images() {
                 .ok();
             }
         }
+        env::set_var("PNG_SIZE", size.to_string());
     });
 }
