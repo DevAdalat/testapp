6927ba9098b959ce310729c228411ee33a5287aa
DevAdalat <akbusiness022@gmail.com>
2022-10-12 05:33:01 +0530

    Testing

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -43,11 +43,12 @@ class HomeController extends GetxController {
     var path = dirPath.toNativeUtf8().cast<ffi.Char>();
     greet.value = lib.get_images_size(path).cast<Utf8>().toDartString();
     lib.rust_cstr_free(path);
-		while (Platform.environment["PNG_SIZE_STATUS"] == "LOADING") {
-			greet.value = "LOADING";
-		}
-		greet.value = Platform.environment["PNG_SIZE"] ?? "Failed";
-	}
+    while (Platform.environment["PNG_SIZE_STATUS"] == "LOADING") {
+      await Future.delayed(200.milliseconds);
+      greet.value = "LOADING";
+    }
+    greet.value = Platform.environment["PNG_SIZE"] ?? "Failed";
+  }
 
   @override
   dispose() {
