65e40596e126a84158f5f95554438927f7e87c7e
DevAdalat <akbusiness022@gmail.com>
2022-10-12 12:06:53 +0530

    Testing

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -31,7 +31,7 @@ pub extern "C" fn rust_cstr_free(s: *mut c_char) {
 }
 
 #[no_mangle]
-pub extern "C" fn get_images_size(path: *mut c_char) -> *mut c_char {
+pub extern "C" fn init(path: *mut c_char) -> *mut c_char {
     env::remove_var("PNG_SIZE");
     let c_str = unsafe { CStr::from_ptr(path) };
     let my_path = match c_str.to_str() {
@@ -64,3 +64,101 @@ fn get_total_size_of_images(path: String) {
         env::set_var("PNG_SIZE_STATUS", "DONE");
     });
 }
+
+pub extern "C" fn is_png_size_work_done() -> bool {
+    if env::var("PNG_SIZE_STATUS").unwrap() == "LOADING" {
+        false
+    } else {
+        true
+    }
+}
+
+pub extern "C" fn get_png_size() -> *mut c_char {
+    CString::new(env::var("PNG_SIZE").unwrap())
+        .unwrap()
+        .into_raw()
+}
+
+//fn get_total_size_of_videos(path: String) {
+//  env::remove_var("PNG_SIZE");
+//  thread::spawn(|| {
+//      let total_size = WalkDir::new(path)
+//          .into_iter()
+//          .filter_map(|entry| entry.ok())
+//          .filter(|e| {
+//              e.file_name().to_string_lossy().ends_with(".mp4")
+//                  || e.file_name().to_string_lossy().ends_with(".3gp")
+//                  || e.file_name().to_string_lossy().ends_with(".mkv")
+//                  || e.file_name().to_string_lossy().ends_with(".avi")
+//                  || e.file_name().to_string_lossy().ends_with(".webm")
+//                  || e.file_name().to_string_lossy().ends_with(".ogg")
+//                  || e.file_name().to_string_lossy().ends_with(".m4v")
+//          })
+//          .filter_map(|entry| entry.metadata().ok())
+//          .filter(|metadata| metadata.is_file())
+//          .fold(0, |acc, m| acc + m.len());
+//      env::set_var("PNG_SIZE", total_size.to_string());
+//      env::set_var("PNG_SIZE_STATUS", "DONE");
+//  });
+//}
+
+//fn get_total_size_of_docs(path: String) {
+//  env::remove_var("PNG_SIZE");
+//  thread::spawn(|| {
+//      let total_size = WalkDir::new(path)
+//          .into_iter()
+//          .filter_map(|entry| entry.ok())
+//          .filter(|e| {
+//              e.file_name().to_string_lossy().ends_with(".jpg")
+//                  || e.file_name().to_string_lossy().ends_with(".word")
+//                  || e.file_name().to_string_lossy().ends_with(".docs")
+//                  || e.file_name().to_string_lossy().ends_with(".html")
+//                  || e.file_name().to_string_lossy().ends_with(".html")
+//          })
+//          .filter_map(|entry| entry.metadata().ok())
+//          .filter(|metadata| metadata.is_file())
+//          .fold(0, |acc, m| acc + m.len());
+//      env::set_var("PNG_SIZE", total_size.to_string());
+//      env::set_var("PNG_SIZE_STATUS", "DONE");
+//  });
+//}
+
+// fn get_total_size_of_apk(path: String) {
+//  env::remove_var("PNG_SIZE");
+//  thread::spawn(|| {
+//      let total_size = WalkDir::new(path)
+//          .into_iter()
+//          .filter_map(|entry| entry.ok())
+//          .filter(|e| {
+//              e.file_name().to_string_lossy().ends_with(".apk")
+//                  || e.file_name().to_string_lossy().ends_with(".xapk")
+//          })
+//          .filter_map(|entry| entry.metadata().ok())
+//          .filter(|metadata| metadata.is_file())
+//          .fold(0, |acc, m| acc + m.len());
+//      env::set_var("PNG_SIZE", total_size.to_string());
+//      env::set_var("PNG_SIZE_STATUS", "DONE");
+//  });
+//}
+
+//fn get_total_size_of_musics(path: String) {
+//  env::remove_var("PNG_SIZE");
+//  thread::spawn(|| {
+//      let total_size = WalkDir::new(path)
+//          .into_iter()
+//          .filter_map(|entry| entry.ok())
+//          .filter(|e| {
+//              e.file_name().to_string_lossy().ends_with(".mp3")
+//                  || e.file_name().to_string_lossy().ends_with(".m4a")
+//                  || e.file_name().to_string_lossy().ends_with(".wav")
+//                  || e.file_name().to_string_lossy().ends_with(".dng")
+//                  || e.file_name().to_string_lossy().ends_with(".bmp")
+//                  || e.file_name().to_string_lossy().ends_with(".gif")
+//          })
+//          .filter_map(|entry| entry.metadata().ok())
+//          .filter(|metadata| metadata.is_file())
+//          .fold(0, |acc, m| acc + m.len());
+//      env::set_var("PNG_SIZE", total_size.to_string());
+//      env::set_var("PNG_SIZE_STATUS", "DONE");
+//  });
+//}
