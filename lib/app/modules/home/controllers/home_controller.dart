bf65f96f2306d38078f55df3545c2d27666a54d3
DevAdalat <akbusiness022@gmail.com>
2022-10-17 17:26:06 +0530

    Testing

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -8,25 +8,29 @@ import 'package:telegram_client/tdlib/tdlib.dart';
 
 class HomeController extends GetxController {
   startService() async {
-    Tdlib tg = Tdlib("libtdjson.so", clientOption: {
-      "api_id": 12860793,
-      "api_hash": "668c3c09dc67819c912c918db5648cf1",
-      'database_directory': "",
-      'files_directory': "",
-      "use_file_database": true,
-      "use_chat_info_database": true,
-      "use_message_database": true,
-      "use_secret_chats": true,
-      'enable_storage_optimizer': true,
-      'system_language_code': 'en',
-      'new_verbosity_level': 0,
-      'application_version': 'v1',
-      'device_model': 'Telegram Client HexaMinate',
-      'system_version': Platform.operatingSystemVersion,
-      "database_key": "",
-      "start": true,
-    });
-	 final clients =	await tg.getMeClients();
-	 Get.snackbar("Info", clients.toString());
+    try {
+      Tdlib tg = Tdlib("libtdjson.so", clientOption: {
+        "api_id": 12860793,
+        "api_hash": "668c3c09dc67819c912c918db5648cf1",
+        'database_directory': "",
+        'files_directory': "",
+        "use_file_database": true,
+        "use_chat_info_database": true,
+        "use_message_database": true,
+        "use_secret_chats": true,
+        'enable_storage_optimizer': true,
+        'system_language_code': 'en',
+        'new_verbosity_level': 0,
+        'application_version': 'v1',
+        'device_model': 'Mi 10i',
+        'system_version': Platform.operatingSystemVersion,
+        "database_key": "",
+        "start": true,
+      });
+      final clients = await tg.getChat(1570907697, clientId: 1570907697);
+      Get.snackbar("Info", clients.toString());
+    } catch (e) {
+      Get.snackbar("Error", e.toString());
+    }
   }
 }
