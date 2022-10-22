60738b010e8dea41ceeb78ca46d0caf1a4f48de5
DevAdalat <akbusiness022@gmail.com>
2022-10-22 17:46:25 +0530

    Testing

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -1,4 +1,6 @@
-import 'dart:convert';
+import 'dart:async';
+import 'dart:ffi';
+import 'dart:isolate';
 
 import 'package:tfile/app/tdLibJson/tdlib_interface.dart';
 import 'package:tfile/app/tdLibJson/tdlibjson_api.dart';
@@ -10,9 +12,24 @@ class SplashController extends GetxController {
     final tg = TdlibInterface();
     const authState = GetAuthorizationState();
     tg.sendData(authState.toJson());
-    tg.receiveData().listen((message) { 
-			final tdData = json.decode(message);
-			CustomSnackbar.customSnackbar(tdData.toString());
+		ReceivePort port = ReceivePort();
+		final isoData = IsolateTdlib(lib: tg.tdJsonNative, client: tg.tdlibClient, port: port.sendPort, timeOut: 10);
+		Isolate.spawn(_isolateTdReceive, isoData);
+		port.listen((message) {
+			CustomSnackbar.customSnackbar(message.toString());
 		});
-  }
+		
+ }
+	_isolateTdReceive(IsolateTdlib isolateTdlib) {
+		final lib = isolateTdlib.lib;
+		final client = isolateTdlib.client;
+		Timer.periodic(100.milliseconds, (timer) {
+			final rawData =  lib.td_json_client_receive(client, isolateTdlib.timeOut);
+			if (rawData != nullptr) {
+				isolateTdlib.port.send(rawData.toDString());
+			} else {
+				isolateTdlib.port.send("Empty data");
+			}
+		});
+}
 }
