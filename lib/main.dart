a9a147dd4590a25f1d65640662bbabeed1aa8a30
DevAdalat <akbusiness022@gmail.com>
2022-11-01 13:40:18 +0530

    Testing

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:e_file/app/my_app.dart';
 import 'package:e_file/app/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   await Future.delayed(10.milliseconds);
+	Hive.initFlutter();
 	await openDB();
 //await EFileStorage().initEFileStorage();
 //await AppSettings.init();
