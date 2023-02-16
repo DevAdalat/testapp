dc7d4484eb8a24684b12e0ce5ccbaebb89f53fd2
DevAdalat <akbusiness022@gmail.com>
2023-02-16 08:08:57 +0530

    test: Testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'dart:ffi';
+// ignore_for_file: use_build_context_synchronously
 
-import 'package:extension_test/extention_utils.dart';
+import 'dart:convert';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:simple_audio/simple_audio.dart';
+import 'package:json_dynamic_widget/json_dynamic_widget.dart';
 
 void main() {
-	SimpleAudio.init();
   runApp(const MyApp());
 }
 
@@ -16,106 +16,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Testapp',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Picker(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _output = "";
-  String _libPath = "";
-  final symController = TextEditingController();
-  final dataController = TextEditingController();
-  final ext = ExtensionFfiImpl(DynamicLibrary.open("libextension.so"));
-	late SimpleAudio audio;
-
-  @override
-  initState() {
-    super.initState();
-		audio = SimpleAudio();
-    FilePicker.platform.clearTemporaryFiles();
-		audio.stop();
-  }
+class Picker extends StatelessWidget {
+  const Picker({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Extension Test")),
-        body: Column(
-          children: [
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                      controller: symController,
-                      decoration: InputDecoration(
-                          hintText: "Symbol Name",
-                          label: const Text("Symbol"),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5),
-                          ))),
-                  const SizedBox(height: 15),
-                  TextField(
-                      controller: dataController,
-                      decoration: InputDecoration(
-                          hintText: "Data (In JSON format)",
-                          label: const Text("Data"),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5),
-                          ))),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Center(
-                      child: TextButton(
-                          onPressed: () async {
-                            FilePickerResult? result =
-                                await FilePicker.platform.pickFiles();
-                            if (result != null) {
-                              _libPath = result.files.first.path!;
-                            } else {
-                              // ignore: use_build_context_synchronously
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content: Text("Please Pick a File")));
-                            }
-                          },
-                          child: const Text("Pick Library"))),
-                  Center(
-                      child: TextButton(
-                          onPressed: () async {
-                            try {
-															audio.open(_libPath);
-															audio.play();
-                            } catch (e) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text(e.toString())));
-                            }
-                          },
-                          child: const Text("Play")))
-                ],
-              ),
-            )),
-            Expanded(child: SingleChildScrollView(child: Text(_output)))
-          ],
-        ));
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            try {
+              final result = await FilePicker.platform.pickFiles();
+              if (result != null) {
+                final registry = JsonWidgetRegistry.instance;
+                final data = json.decode(result.paths.first!);
+                final wid =
+                    JsonWidgetData.fromDynamic(data, registry: registry);
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => FullWidgetPage(data: wid!)));
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Please pick a file')));
+              }
+            } catch (e) {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(e.toString())));
+            }
+          },
+          child: const Text('Pick Json File'),
+        ),
+      ),
+    );
   }
+}
+
+class FullWidgetPage extends StatelessWidget {
+  const FullWidgetPage({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  final JsonWidgetData data;
+
+  @override
+  Widget build(BuildContext context) => data.build(context: context);
 }
